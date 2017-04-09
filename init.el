2a5151e060e42cd320273ec2e9c439b591a1298f
u-yuta <u-yuta@users.noreply.github.com>
2017-04-09 16:19:07 +0900

    Add magit and markdown-mode

--- init.el
+++ init.el
@@ -20,7 +20,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-	(hc-zenburn-theme volatile-highlights zenburn-theme migemo ido-vertical-mode tabbar use-package))))
+	(magit markdown-mode hc-zenburn-theme volatile-highlights zenburn-theme migemo ido-vertical-mode tabbar use-package)))
+ '(tabbar-mode nil nil (tabbar)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
